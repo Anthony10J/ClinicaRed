5045b138439a6e0509ac9e49956603c8a1d87968
Anthony <anthony.duarte@unah.hn>
2024-07-31 14:42:47 -0600

    Revert "Corrección perfil"
    This reverts commit c8813a46f311498cc1eee8830ba800d5778672f8.

--- PHP/Perfil/V_Perfil/V_cambiarcontrasenaPerfil.php
+++ PHP/Perfil/V_Perfil/V_cambiarcontrasenaPerfil.php
@@ -44,8 +44,8 @@
                         <p class="formulario__input-error"></p>
                     </div>
                 </div>
-                <!-- <input class="btn" type="submit" name="register" value="Enviar"><br>  -->
-                <button type="submit" class="btn">Enviar</button>
+                <input class="btn" type="submit" name="register" value="Enviar"><br> 
+               
                  <a href="../V_Perfil/V_perfil.php">Regresar al Perfil</a>
             </form>
         </div>
