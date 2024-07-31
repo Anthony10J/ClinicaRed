8dd4bd5fec66fabffb806e1d2e9829fc631f40ce
Anthony <anthony.duarte@unah.hn>
2024-07-31 01:53:55 -0600

    prueba

--- PHP/Seguridad/Backup/V_respaldo.php
+++ PHP/Seguridad/Backup/V_respaldo.php
@@ -1,4 +1,15 @@
-<?php session_start();?>
+<?php session_start();
+if (!isset($_SESSION["usuario"])) {
+    echo '
+          <script>
+                 alert("Por favor, debes iniciar sesión.")
+                window.location = "/index.php";
+            </script>
+       ';
+    session_destroy(); // Destruye la sesión
+    die(); // el código se detiene en esta línea 
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 
