b15b4c8c3cb4add1ee270fd495d70b3577d0bd1b
Dulce-Diaz <dediazg@unah.hn>
2024-08-02 00:31:52 -0600

    RegistroU del CRUD bitacora
    Registro de las acciones del  modulo usuario en bitacora

--- PHP/Seguridad/Usuario/C_Usuario/C_editar_usuario.php
+++ PHP/Seguridad/Usuario/C_Usuario/C_editar_usuario.php
@@ -1,4 +1,5 @@
 <?php
+include '../../../Seguridad/Roles_permisos/permisos/Obtener_Id_Objeto.php';
 include '../../../Controladores/Conexion/Conexion_be.php';
 // Verificar si la sesión ya está activa
 if (session_status() === PHP_SESSION_ACTIVE) {
@@ -19,6 +20,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $rol = $_POST["rol"];
     $fechanacimiento = $_POST["fechanacimiento"];
     $fechacontratacion = $_POST["fechacontratacion"];
+
+    
+    // obtener el objeto
+$id_objeto = Obtener_Id_Objeto('V_usuario');
+if ($id_objeto === null) {
+    echo "Error: id_objeto es NULL";
+    exit();
+}
+$id_objeto = $conexion->real_escape_string($id_objeto);
+if ($conexion->query("SET @id_objeto = '$id_objeto'") === FALSE) {
+    echo "Error setting id_objeto variable: " . $conexion->error;
+    exit();
+}
+
+
+// Obtener el ID del usuario actual desde la sesión
+if (isset($_SESSION['id_D'])) {
+$current_user_id = $_SESSION['id_D'];
+
+$current_user_id = mysqli_real_escape_string($conexion, $current_user_id);
+
+// Establecer la variable de sesión en QL
+if ($conexion->query("SET @current_user_id = '$current_user_id'") === FALSE) {
+    echo "Error setting session variable: " . $conexion->error;
+    exit();
+}
+} else {
+echo "Error: current_user_id es NULL";
+exit();
+}
+
 
     // Consulta de actualización con marcadores de posición corregidos
     $actualizarUsuarioQuery = "UPDATE tbl_ms_usuario SET DNI = ?, Usuario = ?, Direccion = ?, Correo = ?, Nombre = ?, Estado_Usuario = ?, 
