517746eaf6fd9a7f7def04e27334c8f47790a98a
Dulce-Diaz <dediazg@unah.hn>
2024-08-03 15:06:37 -0600

    registro de pantalla identidad

--- PHP/Seguridad/Mantenimiento_Paciente/V_Identidad/V_modal_identidad.php
+++ PHP/Seguridad/Mantenimiento_Paciente/V_Identidad/V_modal_identidad.php
@@ -7,9 +7,37 @@ if (session_status() === PHP_SESSION_ACTIVE) {
     session_start();
 }
 include '../../../Seguridad/Roles_permisos/permisos/Obtener_Id_Objeto.php';
+include('../../../Controladores/Conexion/Conexion_be.php');
+include('../../../Controladores/bitacora.php');
+
+// Obtener el ID del usuario actual desde la sesión
+if (isset($_SESSION['id_D'])) {
+    $current_user_id = $_SESSION['id_D'];
+    
+    $current_user_id = mysqli_real_escape_string($conexion, $current_user_id);
+    
+    // Establecer la variable de sesión en QL
+    if ($conexion->query("SET @current_user_id = '$current_user_id'") === FALSE) {
+        echo "Error setting session variable: " . $conexion->error;
+        exit();
+    }
+    } else {
+    echo "Error: current_user_id es NULL";
+    exit();
+}
+
 $id_rol = $_SESSION['IdRol'];
 $id_objeto = Obtener_Id_Objeto('V_modal_identidad');
 $Permisos_Objeto = Obtener_Permisos_Rol_Objeto($id_rol, $id_objeto);
+
+if ($id_objeto == "12"){
+    $u=$_SESSION['usuario'];
+    $n= $_SESSION['id_D'];
+    $a='INGRESO A PANTALLA';
+    $d= $_SESSION['usuario'].' INGRESO A LA PANTALLA DE IDENTIDAD.';
+    $o= $id_objeto;
+    bitacora($n,$a,$d,$o); 
+}
 
 if ($Permisos_Objeto["Permiso_Consultar"] !== "1"){
         header("Location: /PHP/Seguridad/Roles_permisos/permisos/V_error_permiso.php");   
