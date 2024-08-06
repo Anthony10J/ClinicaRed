873d60c6649134eac236513f98c7c8c899d5db2c
Anthony <anthony.duarte@unah.hn>
2024-08-06 15:05:44 -0600

    Update V_restore.php

--- PHP/Seguridad/Backup/V_restore.php
+++ PHP/Seguridad/Backup/V_restore.php
@@ -12,13 +12,14 @@ if (!isset($_SESSION["usuario"])) {
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurar Base de Datos</title>
     <link rel="stylesheet" href="./restore.css">
     <!-- Vendor CSS Files -->
-<link href="../../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="../../../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
     <link href="../../../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
     <link href="../../../assets/vendor/quill/quill.snow.css" rel="stylesheet">
@@ -32,9 +33,14 @@ if (!isset($_SESSION["usuario"])) {
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"> </script>
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
+
+    <!-- Alertas -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
+
 <body>
-<?php
+    <?php
     include '../../../Recursos/Componentes/header.php';
     include '../../../Recursos/Componentes/SideBar.html';
     ?>
@@ -45,29 +51,29 @@ if (!isset($_SESSION["usuario"])) {
                 <label for="backup">Selecciona un punto de restauración:</label>
                 <select id="backup" name="restorePoint">
 
-                <option value="" disabled="" selected=""></option>
-			<?php
-				include_once './Connet.php';
-				$ruta=BACKUP_PATH;
-				if(is_dir($ruta)){
-				    if($aux=opendir($ruta)){
-				        while(($archivo = readdir($aux)) !== false){
-				            if($archivo!="."&&$archivo!=".."){
-				                $nombrearchivo=str_replace(".sql", "", $archivo);
-				                $nombrearchivo=str_replace("-", ":", $nombrearchivo);
-				                $ruta_completa=$ruta.$archivo;
-				                if(is_dir($ruta_completa)){
-				                }else{
-				                    echo '<option value="'.$ruta_completa.'">'.$nombrearchivo.'</option>';
-				                }
-				            }
-				        }
-				        closedir($aux);
-				    }
-				}else{
-				    echo $ruta." No es ruta válida";
-				}
-			?>
+                    <option value="" disabled="" selected=""></option>
+                    <?php
+                    include_once './Connet.php';
+                    $ruta = BACKUP_PATH;
+                    if (is_dir($ruta)) {
+                        if ($aux = opendir($ruta)) {
+                            while (($archivo = readdir($aux)) !== false) {
+                                if ($archivo != "." && $archivo != "..") {
+                                    $nombrearchivo = str_replace(".sql", "", $archivo);
+                                    $nombrearchivo = str_replace("-", ":", $nombrearchivo);
+                                    $ruta_completa = $ruta . $archivo;
+                                    if (is_dir($ruta_completa)) {
+                                    } else {
+                                        echo '<option value="' . $ruta_completa . '">' . $nombrearchivo . '</option>';
+                                    }
+                                }
+                            }
+                            closedir($aux);
+                        }
+                    } else {
+                        echo $ruta . " No es ruta válida";
+                    }
+                    ?>
                 </select>
             </div>
             <button type="submit" class="btn1">Restaurar</button>
@@ -76,32 +82,33 @@ if (!isset($_SESSION["usuario"])) {
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-<!-- Vendor JS Files -->
-<script src="../../../assets/vendor/apexcharts/apexcharts.min.js"></script>
-<script src="../../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="../../../assets/vendor/chart.js/chart.umd.js"></script>
-<script src="../../../assets/vendor/echarts/echarts.min.js"></script>
-<script src="../../../assets/vendor/quill/quill.min.js"></script>
-<script src="../../../assets/vendor/simple-datatables/simple-datatables.js"></script>
-<script src="../../../assets/vendor/tinymce/tinymce.min.js"></script>
-<script src="../../../assets/vendor/php-email-form/validate.js"></script>
-<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+    <!-- Vendor JS Files -->
+    <script src="../../../assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="../../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../../../assets/vendor/chart.js/chart.umd.js"></script>
+    <script src="../../../assets/vendor/echarts/echarts.min.js"></script>
+    <script src="../../../assets/vendor/quill/quill.min.js"></script>
+    <script src="../../../assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="../../../assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="../../../assets/vendor/php-email-form/validate.js"></script>
+    <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
 
-<!-- Template Main JS File -->
-<script src="../../../assets/js/main.js"></script>
-<script>
-    document.getElementById('restoreForm').addEventListener('submit', function(event) {
-        var backupSelect = document.getElementById('backup');
-        if (backupSelect.value === '') {
-            event.preventDefault();  // Evita que el formulario se envíe
-            Swal.fire({
-                title: 'Error',
-                text: 'Por favor selecciona la Base de datos a restaurar',
-                icon: 'error',
-                confirmButtonText: 'Aceptar'
-            });
-        }
-    });
-</script>
+    <!-- Template Main JS File -->
+    <script src="../../../assets/js/main.js"></script>
+    <script>
+        document.getElementById('restoreForm').addEventListener('submit', function(event) {
+            var backupSelect = document.getElementById('backup');
+            if (backupSelect.value === '') {
+                event.preventDefault(); // Evita que el formulario se envíe
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Por favor selecciona la Base de datos a restaurar',
+                    icon: 'error',
+                    confirmButtonText: 'Aceptar'
+                });
+            }
+        });
+    </script>
 </body>
+
 </html>
