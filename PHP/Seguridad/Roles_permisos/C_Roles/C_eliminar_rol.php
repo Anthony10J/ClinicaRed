ce99a6b9d92dc30ad86459b94c0d2d238ff87cec
Anthony <anthony.duarte@unah.hn>
2024-08-07 13:28:42 -0600

    Boton eliminar rol, Redireccion editar expediente

---
+++ PHP/Seguridad/Roles_permisos/C_Roles/C_eliminar_rol.php
@@ -0,0 +1,35 @@
+<?php
+include('../../../Controladores/Conexion/Conexion_be.php');
+// Verificar si el ID del rol está presente en la solicitud POST
+if (isset($_POST['idRol'])) {
+    $idRol = $_POST['idRol'];
+
+    // Preparar la consulta para eliminar el rol
+    $sql = "DELETE FROM tbl_ms_roles WHERE Id_Rol = ?";
+
+    // Preparar la sentencia
+    if ($stmt = $conexion->prepare($sql)) {
+        // Vincular el parámetro
+        $stmt->bind_param("i", $idRol);
+
+        // Ejecutar la consulta
+        if ($stmt->execute()) {
+            // Si la eliminación es exitosa, enviar una respuesta exitosa
+            echo 'success';
+        } else {
+            // Si ocurre un error, enviar una respuesta de error
+            echo 'error';
+        }
+
+        // Cerrar la sentencia
+        $stmt->close();
+    } else {
+        echo 'error';
+    }
+
+    // Cerrar la conexión
+    $conexion->close();
+} else {
+    echo 'error';
+}
+?>
