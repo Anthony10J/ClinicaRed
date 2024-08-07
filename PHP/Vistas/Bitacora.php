45e257fe5633e7876728722d2ff2a2b5eccb26d2
Dulce-Diaz <dediazg@unah.hn>
2024-08-07 09:39:44 -0600

    Update Bitacora.php

--- PHP/Vistas/Bitacora.php
+++ PHP/Vistas/Bitacora.php
@@ -185,7 +185,7 @@ include '../Controladores/Conexion/Conexion_be.php';
     border-radius: 10px;
     display: inline-block;
     position: relative;
-    width: 125px; /* Ajusta este valor según el ancho que desees */
+    width: 140px; /* Ajusta este valor según el ancho que desees */
     height: 25px; 
     margin-left: 100px; /* Ajusta este valor según el espacio que desees entre los botones */
      }
