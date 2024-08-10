4a2e1efb1fa3854b56ae3151538d076284567683
AnthoniAvila-UNAH <158218989+AnthoniMatute24@users.noreply.github.com>
2024-08-10 00:03:25 -0600

    Cambios en auto registro, pendiente las validaciones

--- PHP/javascript/validacionAutoRegistro.js
+++ PHP/javascript/validacionAutoRegistro.js
@@ -6,9 +6,9 @@ const icon = document.querySelectorAll('.ver_password');
 const expresiones = {
   usuario: /^[a-zA-Z]{1,15}$/, 
   nombre: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, 
-  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  correo: /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
   direccion: /^[a-zA-ZÀ-ÿ0-9\s,.-]+$/,
-  password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]+$/, 
+  password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{5,}$/, 
   dni: /^(?!00)(?!.*0{5}$)[0-9]{1,13}$/,
   dniN:/^[0-9]{1,13}$/
 };
@@ -95,15 +95,19 @@ let validarInputUsuario = (e) => {
 
   estadoValidacion.estadoCV = funciones.validarCampoVacio(e.target, 'usuario', 'Por favor, ingresa tu nombre de usuario');
 
-  estadoValidacion.estadoCV
-    ? (estadoValidacion.estadoER = funciones.validarExpresionRegular(expresiones.usuario, e.target, 'usuario', 'Solo se permiten letras')) : "";
+  if (estadoValidacion.estadoCV) {
+    estadoValidacion.estadoER = funciones.validarExpresionRegular(expresiones.usuario, e.target, 'usuario', 'Solo se permiten letras');
+  }
 
-  estadoValidacion.estadoER
-    ? (estadoValidacion.estadoMC = funciones.validarMismoCaracter(e.target, 'usuario', 'No debe colocar el mismo caracter +2 veces seguidas')) : "";
- 
-  estadoValidacion.estadoMC
-    ? (estadoValidacion.estadoUE = funciones.validarEspacios(/\s\s/g, e.target, 'usuario', 'Debe limitarse a un espacio')) : "";
-    return estadoValidacion;
+  if (estadoValidacion.estadoER) {
+    estadoValidacion.estadoMC = funciones.validarMismoCaracter(e.target, 'usuario', 'No debe colocar el mismo caracter más de 2 veces seguidas');
+  }
+
+  if (estadoValidacion.estadoMC) {
+    estadoValidacion.estadoUE = funciones.validarEspacios(/\s\s+/g, e.target, 'usuario', 'Debe limitarse a un solo espacio entre palabras');
+  }
+
+  return estadoValidacion;
 };
 
 let validarInputDNI = (e) => {
@@ -112,19 +116,17 @@ let validarInputDNI = (e) => {
     estadoER: false,
     estadoCC: false,
     estadoCM: false,
-    
   };
   estadoValidacion.estadoCV = funciones.validarCampoVacio(e.target, 'dni', 'Por favor, ingresa tu DNI');
-  estadoValidacion.estadoCV
-
-    ? (estadoValidacion.estadoCC = funciones.validarCerosConsecutivos(expresiones.dni, e.target, "dni", 'DNI no válido')) :"";
-  estadoValidacion.estadoCC
-
-    ? (estadoValidacion.estadoCM = funciones.validarCampoMinimo(e.target, 'dni', 'Por favor, ingresa correctamente el DNI')) :"";
-  estadoValidacion.estadoCM
-
-    ? (estadoValidacion.estadoER = funciones.validarExpresionRegular(expresiones.dniN, e.target, 'dni', 'Solo se permiten números')) :"";
-
+  if (estadoValidacion.estadoCV) {
+    estadoValidacion.estadoCC = funciones.validarCerosConsecutivos(expresiones.dni, e.target, "dni", 'DNI no válido');
+  }
+  if (estadoValidacion.estadoCC) {
+    estadoValidacion.estadoCM = funciones.validarCampoMinimo(e.target, 'dni', 'Por favor, ingresa correctamente el DNI');
+  }
+  if (estadoValidacion.estadoCM) {
+    estadoValidacion.estadoER = funciones.validarExpresionRegular(expresiones.dniN, e.target, 'dni', 'Solo se permiten números');
+  }
   return estadoValidacion;
 };
 
@@ -136,17 +138,16 @@ let validarInputNombre = (e) => {
     estadoUE: false,
   };
   estadoValidacion.estadoCV = funciones.validarCampoVacio(e.target, 'nombre', 'Por favor, ingresa tu nombre');
-
-  estadoValidacion.estadoCV
-    ? (estadoValidacion.estadoER = funciones.validarExpresionRegular(expresiones.nombre, e.target, 'nombre', 'Solo se permiten letras')) : "";
-
-    estadoValidacion.estadoER
-    ? (estadoValidacion.estadoUE = funciones.validarEspacios(/\s\s/g, e.target, 'nombre', 'Debe limitarse a un espacio')) : "";
-
-  estadoValidacion.estadoUE
-    ? (estadoValidacion.estadoMC = funciones.validarMismoCaracter(e.target, 'nombre', 'No debe colocar el mismo caracter +2 veces seguidas')) : "";
-
-    return estadoValidacion;
+  if (estadoValidacion.estadoCV) {
+    estadoValidacion.estadoER = funciones.validarExpresionRegular(expresiones.nombre, e.target, 'nombre', 'Solo se permiten letras');
+  }
+  if (estadoValidacion.estadoER) {
+    estadoValidacion.estadoUE = funciones.validarEspacios(/\s\s+/g, e.target, 'nombre', 'Debe limitarse a un solo espacio entre palabras');
+  }
+  if (estadoValidacion.estadoUE) {
+    estadoValidacion.estadoMC = funciones.validarMismoCaracter(e.target, 'nombre', 'No debe colocar el mismo caracter más de 2 veces seguidas');
+  }
+  return estadoValidacion;
 };
 
 let validarInputDireccion = (e) => {
@@ -158,40 +159,32 @@ let validarInputDireccion = (e) => {
     estadoCM: false,
   };
   estadoValidacion.estadoCV = funciones.validarCampoVacio(e.target, 'direccion', 'Por favor, ingresa tu dirección');
-
-  estadoValidacion.estadoCV
-  ? (estadoValidacion.estadoUE = funciones.validarEspacios(/\s\s/g, e.target, 'direccion', 'Debe limitarse a un espacio')) : "";
-
-  estadoValidacion.estadoUE
-    ? (estadoValidacion.estadoER = funciones.validarExpresionRegular(expresiones.direccion, e.target, 'direccion', 'Caracter no válido')) : "";
-
-  estadoValidacion.estadoER
-    ? (estadoValidacion.estadoMC = funciones.validarMismoCaracter(e.target, 'direccion', 'No debe colocar el mismo caracter +2 veces seguidas')) : "";
-    estadoValidacion.estadoMC
-    ? (estadoValidacion.estadoCM = funciones.validarCampoMaximo (e.target, "direccion", "No cumple la cantidad mínima de caracteres")): "";
-     
-    return estadoValidacion;
+  if (estadoValidacion.estadoCV) {
+    estadoValidacion.estadoUE = funciones.validarEspacios(/\s\s+/g, e.target, 'direccion', 'Debe limitarse a un solo espacio entre palabras');
+  }
+  if (estadoValidacion.estadoUE) {
+    estadoValidacion.estadoER = funciones.validarExpresionRegular(expresiones.direccion, e.target, 'direccion', 'Caracter no válido');
+  }
+  if (estadoValidacion.estadoER) {
+    estadoValidacion.estadoMC = funciones.validarMismoCaracter(e.target, 'direccion', 'No debe colocar el mismo caracter más de 2 veces seguidas');
+  }
+  if (estadoValidacion.estadoMC) {
+    estadoValidacion.estadoCM = funciones.validarCampoMaximo(e.target, "direccion", "No cumple la cantidad mínima de caracteres");
+  }
+  return estadoValidacion;
 };
-
 
 export const validarFechaNacimiento = () => {
   const fechaNacimientoInput = document.getElementById('fechanacimiento');
   const fechaNacimientoValue = new Date(fechaNacimientoInput.value);
   const fechaActual = new Date();
 
-  // Ajustar la fecha de nacimiento para comparar con la fecha actual
   const fechaNacimientoValida = fechaNacimientoValue <= fechaActual;
-
-  // Calcular la edad
   const edad = fechaActual.getFullYear() - fechaNacimientoValue.getFullYear();
-
-  // Verificar si la fecha de nacimiento es válida (no es posterior a la fecha actual y tiene al menos 18 años)
   const fechaValida = fechaNacimientoValida && edad >= 18;
 
-  // Obtener el elemento del mensaje de error
   const mensajeFechaNacimiento = document.getElementById("mensajeFechaNacimiento");
 
-  // Mostrar mensaje de error si la fecha de nacimiento no es válida
   if (fechaNacimientoInput.value === '') {
       mensajeFechaNacimiento.innerText = 'Ingrese su fecha de nacimiento';
       fechaNacimientoInput.classList.remove('mensaje_error');
@@ -200,66 +193,51 @@ export const validarFechaNacimiento = () => {
           mensajeFechaNacimiento.innerText = '*Fecha no válida';
           fechaNacimientoInput.classList.add('mensaje_error');
       } else {
-          // Si la fecha es válida, eliminamos el mensaje de error y permitimos el envío del formulario
           mensajeFechaNacimiento.innerText = '';
           fechaNacimientoInput.classList.remove('mensaje_error');
-          return true; // La fecha es válida
+          return true;
       }
   }
-  
 }
-// Agregamos un listener al evento submit del formulario para que llame a la función de validación
+
 const formulario = document.getElementById('registerForm'); 
 formulario.addEventListener("submit", function (event) {
-    // Llamamos a la función de validación de fecha de nacimiento
     const fechaValida = validarFechaNacimiento();
   
-    // Comprobamos si la fecha de nacimiento es válida
     if (!fechaValida) {
-        event.preventDefault(); // Detener el envío del formulario
-        // MostrarAlerta('', '¡ERROR!', 'Hay errores en el formulario. Por favor, corrígelos antes de enviarlo.');
+        event.preventDefault();
     }   
 });
-// Agregar un event listener al input de fecha de nacimiento para validarla cuando cambie su valor
+
 document.getElementById("fechanacimiento").addEventListener("change", validarFechaNacimiento);
 
 const validarGenero = () => {
   const generoInput = document.getElementById('mensajeGenero1');
   const generoValue = generoInput.value;
 
-  // Verificar si se ha seleccionado un género válido
   const generoValido = generoValue !== '0';
 
-  // Obtener el elemento del mensaje de error
-  const mensajeGenero = document.getElementById("mensajeGenero2");
-  
-  // Mostrar mensaje de error si el género no es válido
+  const mensajeGenero = document.getElementById("mensajeGenero");
+
   if (!generoValido) {
-      mensajeGenero.innerText = '*Por favor, seleccione un género';
-      generoInput.classList.add('mensaje_error');
+    mensajeGenero.innerText = 'Seleccione una opción válida';
+    return false;
   } else {
-      // Si el género es válido, eliminamos el mensaje de error
-      mensajeGenero.innerText = '';
-      generoInput.classList.remove('mensaje_error');
-      return true; // El género es válido
+    mensajeGenero.innerText = '';
+    return true;
   }
 }
 
-// Agregar un listener al evento change del campo de género para llamar a la función de validación
-const generoInput = document.getElementById('mensajeGenero1');
-generoInput.addEventListener("change", validarGenero);
-// Agregamos un listener al evento submit del formulario para que llame a la función de validación
-const formulario2 = document.getElementById('registerForm'); 
-formulario2.addEventListener("submit", function (event) {
-    // Llamamos a la función de validación de género
-    const generoValido = validarGenero();
-  
-    // Comprobamos si el género es válido
-    if (!generoValido) {
-        event.preventDefault(); // Detener el envío del formulario
-        // MostrarAlerta('', '¡ERROR!', 'Hay errores en el formulario. Por favor, corrígelos antes de enviarlo.');
-    }   
+document.getElementById('mensajeGenero1').addEventListener("change", validarGenero);
+
+formulario.addEventListener("submit", function (event) {
+  const generoValido = validarGenero();
+
+  if (!generoValido) {
+    event.preventDefault();
+  }
 });
+
 
 
 // -----------------------------------
@@ -327,4 +305,3 @@ formulario_Registro.addEventListener('submit', function (e) {
     funciones.MostrarAlerta('error', '¡ERROR!', 'Hay errores en el formulario. Por favor, corrígelos antes de enviarlo.');
   }
 });
-
