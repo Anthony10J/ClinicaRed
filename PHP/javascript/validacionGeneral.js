279c24e3876051370c2293e8ba63945666c508be
Dulce-Diaz <dediazg@unah.hn>
2024-08-10 00:37:31 -0600

    Update validacionGeneral.js

--- PHP/javascript/validacionGeneral.js
+++ PHP/javascript/validacionGeneral.js
@@ -252,3 +252,39 @@ export const validarCerosConsecutivos = (expresion, input, campo, mensajeError) 
 //         MostrarAlerta('', '¡ERROR!', 'Hay errores en el formulario. Por favor, corrígelos antes de enviarlo.');
 //     }   
 // });
+
+export const validarCampoMinimo = (input, campo, mensajeError) => {
+    let elemento = document.getElementById(`grupo__${campo}`);
+
+    if (input.value.trim().length < 13) {
+        elemento.classList.add('formulario__grupo-incorrecto');
+        elemento.classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).textContent = mensajeError;
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos[campo] = false;
+    } else {
+        elemento.classList.remove('formulario__grupo-incorrecto');
+        elemento.classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        campos[campo] = true;
+    }
+    return campos[campo];
+}
+
+export const validarCampoMaximo = (input, campo, mensajeError) => {
+    let elemento = document.getElementById(`grupo__${campo}`);
+
+    if (input.value.trim().length < 5) {
+        elemento.classList.add('formulario__grupo-incorrecto');
+        elemento.classList.remove('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).textContent = mensajeError;
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+        campos[campo] = false;
+    } else {
+        elemento.classList.remove('formulario__grupo-incorrecto');
+        elemento.classList.add('formulario__grupo-correcto');
+        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+        campos[campo] = true;
+    }
+    return campos[campo];
+}
